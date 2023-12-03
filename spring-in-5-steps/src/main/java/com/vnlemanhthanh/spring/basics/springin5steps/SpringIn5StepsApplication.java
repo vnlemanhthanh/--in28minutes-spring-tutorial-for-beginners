10bccb13048b9e6a141d8d8acbf4fb7f42b1a470
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-03 23:55:53 +0700

    Module: Spring in 5 steps

---
+++ spring-in-5-steps/src/main/java/com/vnlemanhthanh/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.spring.basics.springin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn5StepsApplication.class, args);
+	}
+
+}
