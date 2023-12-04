0325106726a5fb275c537358b97226142f3cc099
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-05 00:53:16 +0700

    Spring Injection using Java Constructor and Setter Methods

--- spring-in-5-steps/src/main/java/com/vnlemanhthanh/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/vnlemanhthanh/spring/basics/springin5steps/BinarySearchImpl.java
@@ -9,14 +9,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
-
-    @Autowired
     private SortAlgorithm sortAlgorithm;
 
+    @Autowired
     public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
         super();
         this.sortAlgorithm = sortAlgorithm;
     }
+
+//    @Autowired
+//    public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+//        this.sortAlgorithm = sortAlgorithm;
+//    }
 
     public int binarySearch(int[] numbers, int numberToSearchFor) {
         int[] sortedNumbers  = sortAlgorithm.sort(numbers);
