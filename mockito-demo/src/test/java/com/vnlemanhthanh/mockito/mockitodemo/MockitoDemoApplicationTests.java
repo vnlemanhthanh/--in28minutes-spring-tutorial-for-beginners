52ef98b0e9ae5497e66e5ae06a63932bc38a0d58
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-06 05:16:35 +0700

    Mockito init

---
+++ mockito-demo/src/test/java/com/vnlemanhthanh/mockito/mockitodemo/MockitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.mockito.mockitodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
