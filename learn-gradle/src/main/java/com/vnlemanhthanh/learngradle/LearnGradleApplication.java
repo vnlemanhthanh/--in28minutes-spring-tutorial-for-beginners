c643379665aa1a005643f81b50a96fa2436531d2
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-05 14:38:41 +0700

    Gradle

---
+++ learn-gradle/src/main/java/com/vnlemanhthanh/learngradle/LearnGradleApplication.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.learngradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGradleApplication.class, args);
+	}
+
+}
---
+++ learn-gradle/src/test/java/com/vnlemanhthanh/learngradle/LearnGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.learngradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
