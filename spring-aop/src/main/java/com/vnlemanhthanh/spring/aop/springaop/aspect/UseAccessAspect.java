9f40890821d80779b1a208ad2cfee34ac29f0b0d
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-07 16:59:25 +0700

    Step 03 - Defining an @Before advice. Completed #47

---
+++ spring-aop/src/main/java/com/vnlemanhthanh/spring/aop/springaop/aspect/UseAccessAspect.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+//AOP
+//Configuration
+@Aspect
+@Configuration
+public class UseAccessAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    //What kind of method calls I would intercept
+    //execution(* PACKAGE.*.*(...))
+    @Before("execution (* com.vnlemanhthanh.spring.aop.springaop.business.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        logger.info(" Check for user access ");
+        logger.info(" Allowed execution for {}", joinPoint);
+    }
+
+}
