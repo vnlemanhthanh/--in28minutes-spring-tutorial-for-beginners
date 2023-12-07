6e2ee77204c3c958af89a4ad12c3ce7aed5496e0
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-07 07:57:39 +0700

    Step 04 - Build a Hello World API with Spring Boot. Close #34

---
+++ learn-spring-boot/src/main/java/com/vnlemanhthanh/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.springboot.learnspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping({"/course", "/course.html"})
+    public List<Course> retrieveAllCourse() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "vnlemanhthanh"),
+                new Course(2, "Learn DevOps", "vnlemanhthanh")
+        );
+    }
+}
