8879f6e8bfc3f4befdf06dca469f058cec5ff1fe
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-08 12:58:11 +0700

    Step 04 - Populate data into Person Table. Close #58

--- database-demo/src/main/resources/schema.sql
+++ database-demo/src/main/resources/schema.sql
@@ -11,3 +11,11 @@ create table person
     primary key(id)
 );
 
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10001,  'Ranga', 'Hyderabad', CURRENT_DATE());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10002,  'James', 'New York', CURRENT_DATE());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10003,  'Pieter', 'Amsterdam', CURRENT_DATE());
+
+
